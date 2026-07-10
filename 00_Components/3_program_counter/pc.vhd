055de3b5aec54304024f59e8c89e336cb219fb38
Isaias <isaias.r0423@gmail.com>
2026-07-09 21:45:22 -0500

    program counter complete

---
+++ 00_Components/3_program_counter/pc.vhd
@@ -0,0 +1,33 @@
+-- July 7, 2026
+-- Isaias M Ramirez
+-- The circuit described within this file
+-- is a generic program counter that defaults to 32-bits
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity pc is 
+    generic( num_bits : integer := 32 );
+    port(
+        PCNext  :   in  STD_LOGIC_VECTOR ( num_bits - 1 downto 0 );
+        reset   :   in  STD_LOGIC;
+        clk     :   in  STD_LOGIC;
+        PC      :   out STD_LOGIC_VECTOR ( num_bits - 1 downto 0 )
+    );
+end pc;
+
+architecture behavioral_arc of pc is
+    signal default_instruction : STD_LOGIC_VECTOR ( num_bits - 1 downto 0 ) := (others => '0');
+begin
+    
+    pass_value : process(clk) is
+    begin
+        if rising_edge(clk) then 
+            if(reset) then
+                PC <= default_instruction or X"00001000";
+            else
+                PC <= PCNext;
+            end if;
+        end if;
+    end process pass_value;
+
+end architecture behavioral_arc;

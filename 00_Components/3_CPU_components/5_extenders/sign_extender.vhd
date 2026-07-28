98c9919b8b47dc9b02bcc1cd39134c468da48c44
Isaias <isaias.r0423@gmail.com>
2026-07-27 19:28:11 -0500

    incorporated sw function into sign extender and fixed bugs

--- 00_Components/3_CPU_components/5_extenders/sign_extender.vhd
+++ 00_Components/3_CPU_components/5_extenders/sign_extender.vhd
@@ -10,14 +10,21 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity sign_extender is 
     port (
-        num_in   :   in  STD_LOGIC_VECTOR   ( 24 downto 0);
+        num_in   :   in  STD_LOGIC_VECTOR   ( 23 downto 0);
+        ImmSrc   :   in STD_LOGIC;
         num_ext  :   out STD_LOGIC_VECTOR   ( 31 downto 0)
     );
 end sign_extender;
 
 architecture rtl of sign_extender is
+    signal imm : STD_LOGIC_VECTOR ( 11 downto 0);
 begin
     -- Replicate the input sign bit into the upper bits, then concatenate
     -- with the to form a 32-bit signed-extended result.
-    num_ext <= (31 downto 25 => num_in(num_in'high)) & num_in;
+    -- shift all instruction format indecies to the right by 8 bits
+    with(ImmSrc) select 
+        imm <=  num_in(23 downto 12) when '0',                          -- lw
+                (num_in(23 downto 17) & num_in(4 downto 0)) when '1',   -- sw
+                (others => '0') when others;
+    num_ext <=  ((31 downto 12 => imm(imm'high)) & imm);
 end rtl;
